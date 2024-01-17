46a4486feba118f981dbe551b871855a7a405eea
@hexlet/cli <support@hexlet.io>
2024-01-17 11:29:40 +0300

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -2,90 +2,91 @@ package exercise;
 
 import java.lang.reflect.Field;
 import java.util.*;
+import java.util.stream.Collectors;
+
 
 // BEGIN
 class Validator {
+
     public static List<String> validate(Address address) {
-        //создаю результирующий список:
-        List<String> result = new ArrayList<>();
-
         //получаю доступ к приватным полям класса:
-        Field[] fields = address.getClass().getDeclaredFields();
+        List<Field> fields = List.of(address.getClass().getDeclaredFields());
 
-        //прохожу циклом по массиву с полями:
-        for (Field field : fields) {
-            //разрешение для приватного поля
-            field.setAccessible(true);
-
-            //проверяю наличие аннотации у поля:
-            if (field.isAnnotationPresent(NotNull.class)) {
-
-                try {
-                    if (Objects.isNull(field.get(address))) {
-                        result.add(field.getName());
+        //возвращаю сразу результирующий список:
+        return fields.stream()
+                .filter(field -> field.isAnnotationPresent(NotNull.class))
+                .filter(field -> {
+                    Object value;
+                    try {
+                        field.setAccessible(true);
+                        value = field.get(address);
+                        field.setAccessible(false);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
                     }
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-        return result;
+                    return Objects.isNull(value);
+                })
+                .map(Field::getName)
+                .collect(Collectors.toList());
     }
 
     public static Map<String, List<String>> advancedValidate(Address address) {
 
         Map<String, List<String>> result = new HashMap<>();
 
-        //список с именами полей, не прошедших валидацию с NotNull:
-        List<String> fieldNames = validate(address);
+        List<Field> fields = List.of(address.getClass().getDeclaredFields());
 
-        Field[] fields = address.getClass().getDeclaredFields();
-
-        for (Field field : fields) {
-            field.setAccessible(true);
-
-            //если поле содержит обе аннотацияи (NotNull, MinLength):
-            if (field.isAnnotationPresent(MinLength.class)) {
-
-                try {
-                    List<String> messages = getMessagesOfAllAnnotation(address, field);
-                    if (!messages.isEmpty()) {
-                        result.put(field.getName(), messages);
+        fields.stream()
+                .filter(field ->
+                        field.isAnnotationPresent(NotNull.class) ||
+                        field.isAnnotationPresent(MinLength.class))
+                .forEach(field -> {
+                    String fieldName = field.getName();
+                    List<String> errors;
+                    try {
+                        errors = getMessagesOfAllAnnotation(address, field);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
                     }
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            //если у поля только NotNull:
-            else {
-                if (fieldNames.contains(field.getName())) {
-                    result.put(field.getName(), List.of("can not be null"));
-                }
-            }
-        }
+                    if (!errors.isEmpty()) {
+                        result.put(fieldName, errors);
+                    }
+                });
         return result;
     }
 
-    //метод возвращает список сообщений у полей, не прошедших валидацию с MinLength:
+    //мвозвращает список сообщений у полей, не прошедших валидацию с обеими аннотациями:
     public static List<String> getMessagesOfAllAnnotation(Address address, Field field)
             throws IllegalAccessException {
 
         List<String> messages = new ArrayList<>();
 
+        //инициализирую имя поля:
+        String nameField;
+        try {
+            field.setAccessible(true);
+            nameField = (String) field.get(address);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         //список с именами полей, не прошедших валидацию с NotNull:
-        List<String> fieldNames = validate(address);
-        //получаю значение у аннотации:
-        int value = field.getAnnotation(MinLength.class).minLength();
-        //получаю длину у значения поля:
-        int nameLength = String.valueOf(field.get(address)).length();
+        List<String> fieldsWithNull = validate(address);
 
-        if (fieldNames.contains(field.getName())) {
+        if (fieldsWithNull.contains(field.getName())) {
             messages.add("can not be null");
+        }
 
-            if (nameLength < value) {
+        //список с именами полей, не прошедших валидацию с MinLength:
+        if (field.isAnnotationPresent(MinLength.class)) {
+            //получаю значение у аннотации:
+            int value = field.getAnnotation(MinLength.class).minLength();
+
+            if (nameField == null || nameField.length() < value) {
                 messages.add("length less than " + value);
             }
         }
+
         return messages;
     }
 }
