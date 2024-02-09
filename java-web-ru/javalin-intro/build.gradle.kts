a20ebce50d5fe3ce045ec2c49d15951ce5c367c5
@hexlet/cli <support@hexlet.io>
2024-02-09 19:52:19 +0300

    submit java-web-ru/javalin-intro

---
+++ java-web-ru/javalin-intro/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+    id("com.github.ben-manes.versions") version "0.48.0"
+    application
+    id("java")
+}
+
+application {
+    mainClass.set("exercise.App")
+}
+
+group = "exercise"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // BEGIN
+    
+    // END
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("com.konghq:unirest-java:3.13.0")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    implementation("io.javalin:javalin:5.6.1")
+    implementation("io.javalin:javalin-rendering:5.6.0")
+    implementation("gg.jte:jte:3.0.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
+}
+
---
+++ java-web-ru/javalin-intro/src/main/java/exercise/App.java
@@ -0,0 +1,28 @@
+package exercise;
+
+// BEGIN
+import io.javalin.Javalin;
+// END
+
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        // BEGIN
+        // Создаем приложение
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+        app.get("/", ctx -> ctx.result("Welcome to Hexlet!"));
+
+        // Возвращаем настроенное приложение
+        return app;
+        // END
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);// Стартуем веб-сервер
+    }
+}
