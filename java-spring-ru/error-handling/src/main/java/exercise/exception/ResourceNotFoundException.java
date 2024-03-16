ca2b397f18458ce0fd060bdf303a6df4cde144e2
@hexlet/cli <support@hexlet.io>
2024-03-16 18:16:39 +0300

    submit java-spring-ru/error-handling

---
+++ java-spring-ru/error-handling/src/main/java/exercise/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package exercise.exception;
+
+// BEGIN
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
+// END
